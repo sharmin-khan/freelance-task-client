7164cf2b2d29bb9d1d65f2d84ae341a715a4841f
sharmin-khan <sharminkhancse@gmail.com>
2025-07-08 20:55:53 +0600

    Install react lottie. integrated react-lottie animation with signup and login form layout

--- src/Pages/Signup/Signup.jsx
+++ src/Pages/Signup/Signup.jsx
@@ -1,80 +1,93 @@
 import React from "react";
+import signup from "../../assets/images/register.json";
 import { Link } from "react-router";
+import Lottie from "lottie-react";
 
 const Register = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900">
-      <form className="bg-white dark:bg-gray-800 p-6 rounded shadow-md w-full max-w-md">
-        <h2 className="text-2xl font-bold mb-4 text-center text-blue-600">
-          Create Your Account
-        </h2>
-        <label className="label mb-1">Full Name</label>
-        <input
-          type="text"
-          placeholder="Enter your full name"
-          className="input input-bordered w-full mb-4"
-        />
-        <label className="label mb-1">Email Address</label>
-        <input
-          type="email"
-          placeholder="Enter your email"
-          className="input input-bordered w-full mb-4"
-        />
-        <label className="label mb-1">Photo URL</label>
-        <input
-          type="url"
-          placeholder="Link to your profile photo"
-          className="input input-bordered w-full mb-4"
-        />
-        <label className="label mb-1">Password</label>
-        <input
-          type="password"
-          placeholder="Create a secure password"
-          className="input input-bordered w-full mb-4"
-        />
-        <button type="submit" className="btn btn-primary w-full text-lg">
-          Sign Up
-        </button>
-        <div className="divider">or</div>
-        <div className="text-center mb-3">
-          <button className="btn bg-white text-black border-[#e5e5e5]">
-            <svg
-              aria-label="Google logo"
-              width="16"
-              height="16"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-            >
-              <g>
-                <path d="m0 0H512V512H0" fill="#fff"></path>
-                <path
-                  fill="#34a853"
-                  d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
-                ></path>
-                <path
-                  fill="#4285f4"
-                  d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
-                ></path>
-                <path
-                  fill="#fbbc02"
-                  d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
-                ></path>
-                <path
-                  fill="#ea4335"
-                  d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
-                ></path>
-              </g>
-            </svg>
-            Sign up with Google
+      {/* Form on the left */}
+      <div className="w-1/2 ">
+        <form className="bg-white ml-76 dark:bg-gray-800 p-6 rounded shadow-md w-full max-w-md">
+          <h2 className="text-2xl font-bold mb-4 text-center text-blue-600">
+            Create Your Account
+          </h2>
+          <label className="label mb-1">Full Name</label>
+          <input
+            type="text"
+            placeholder="Enter your full name"
+            className="input input-bordered w-full mb-4"
+          />
+          <label className="label mb-1">Email Address</label>
+          <input
+            type="email"
+            placeholder="Enter your email"
+            className="input input-bordered w-full mb-4"
+          />
+          <label className="label mb-1">Photo URL</label>
+          <input
+            type="url"
+            placeholder="Link to your profile photo"
+            className="input input-bordered w-full mb-4"
+          />
+          <label className="label mb-1">Password</label>
+          <input
+            type="password"
+            placeholder="Create a secure password"
+            className="input input-bordered w-full mb-4"
+          />
+          <button type="submit" className="btn btn-primary w-full text-lg">
+            Sign Up
           </button>
-        </div>
-        <p className="mt-4 text-sm text-center">
-          Already have an account?{" "}
-          <Link to="/login" className="text-blue-500 underline">
-            Log In
-          </Link>
-        </p>
-      </form>
+          <div className="divider">or</div>
+          <div className="text-center mb-3">
+            <button className="btn bg-white text-black border-[#e5e5e5]">
+              <svg
+                aria-label="Google logo"
+                width="16"
+                height="16"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+              >
+                <g>
+                  <path d="m0 0H512V512H0" fill="#fff"></path>
+                  <path
+                    fill="#34a853"
+                    d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
+                  ></path>
+                  <path
+                    fill="#4285f4"
+                    d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
+                  ></path>
+                  <path
+                    fill="#fbbc02"
+                    d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
+                  ></path>
+                  <path
+                    fill="#ea4335"
+                    d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
+                  ></path>
+                </g>
+              </svg>
+              Sign up with Google
+            </button>
+          </div>
+          <p className="mt-4 text-sm text-center">
+            Already have an account?{" "}
+            <Link to="/login" className="text-blue-500 underline">
+              Log In
+            </Link>
+          </p>
+        </form>
+      </div>
+      {/* Lottie animation on the right */}
+      <div className=" hidden lg:block w-1/2">
+        <Lottie
+          animationData={signup}
+          loop={true}
+          className="max-w-md w-full h-[600px] ml-10"
+        />
+      </div>
     </div>
   );
 };
